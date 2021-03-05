4188d514d808f209e9299860937535586bc7aa6f
Тигран <tigr.kman@gmail.com>
2021-03-05 15:29:41 +0300

    запрос на сервер и вывод в консоль данных

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello')
+// Get Quote From API
+async function getQuote () {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data)
+    } catch (e) {
+        getQuote()
+        throw new Error(e)
+    }
+}
+
+// On load
+getQuote();
