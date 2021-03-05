cf17578426e356918f182602c79e17ded5c99886
Тигран <tigr.kman@gmail.com>
2021-03-05 15:31:31 +0300

    тест без proxy

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function getQuote () {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(proxyUrl + apiUrl);
+        const response = await fetch(apiUrl);
         const data = await response.json();
         console.log(data)
     } catch (e) {
